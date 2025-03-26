d3bd3f09cfb5512e004bcc47f9922d0374cc7c69
fatima <fatimaejaz2k18@gmail.com>
2025-03-27 00:04:05 +0530

    changes committed

--- src/main/java/com/youtube/central/Service/RabbitMqService.java
+++ src/main/java/com/youtube/central/Service/RabbitMqService.java
@@ -1,11 +1,12 @@
 package com.youtube.central.Service;
 
+import com.youtube.central.dto.NotificationMessage;
 import org.springframework.amqp.core.Message;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMqService {
-    public void sendNotificationToQueue(Message message){
+    public void sendNotificationMessageToQueue(NotificationMessage message){
 
 
     }
