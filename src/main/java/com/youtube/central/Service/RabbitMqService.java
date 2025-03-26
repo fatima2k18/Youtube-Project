7a9c085ea57901ff7c78d37076e8ba8825dde631
fatima <fatimaejaz2k18@gmail.com>
2025-03-26 20:54:53 +0530

    changes committed

---
+++ src/main/java/com/youtube/central/Service/RabbitMqService.java
@@ -0,0 +1,13 @@
+package com.youtube.central.Service;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMqService {
+    public void sendNotificationToQueue(Message message){
+
+
+    }
+}
+
---
+++ src/main/java/com/youtube/central/dto/NotificationMessage.java
@@ -0,0 +1,7 @@
+package com.youtube.central.dto;
+
+public class NotificationMessage {
+    String purpose; //user-registration, video-upload -notification, new-video-available
+    String email; //user we need to send notification
+
+}
