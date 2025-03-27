9cb47b148aae55c0729ac62497e7645d14658671
fatima <fatimaejaz2k18@gmail.com>
2025-03-27 21:25:16 +0530

    changes committed

--- src/main/java/com/youtube/central/models/userTagHistory.java
+++ src/main/java/com/youtube/central/models/userTagHistory.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.repository.cdi.Eager;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,7 +16,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @ToString
 
-public class userTagHiistory {
+public class userTagHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
